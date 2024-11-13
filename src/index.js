e78e1aa57c7731cfbcb9a935c8f0c5c362f8a665
k-hain <155682079+k-hain@users.noreply.github.com>
2024-11-13 22:43:13 +0100

    feat: rework navbar style

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import './style.css';
 import { createHomeContent } from './home.js';
 import { createMenuContent } from './menu.js';
 import { createAboutContent } from './about.js';
+import logoSvg from './svg/ramen_dining_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg';
 
 class displayController {
   static #divContent = document.querySelector('#content');
   static #nav = document.querySelector('nav');
+  static #logoIcon = document.querySelector('img#logo');
   static #homeButton = this.#nav.querySelector('button#home');
   static #menuButton = this.#nav.querySelector('button#menu');
   static #aboutButton = this.#nav.querySelector('button#about');
 
   static initializePage() {
+    this.#logoIcon.src = logoSvg;
     this.#loadContent(createHomeContent);
     
     this.#homeButton.addEventListener(
